7baeb05577e34e6c3cfbe4aa3af03b2a5d200c00
Parsa <parsarahimnia@gmail.com>
2025-06-26 22:03:28 -0400

    Add Spring Boot backend and React frontend setup

---
+++ backend/src/main/java/com/parsa/movie_watchlist_backend/MovieWatchlistBackendApplication.java
@@ -0,0 +1,13 @@
+package com.parsa.movie_watchlist_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieWatchlistBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieWatchlistBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/parsa/movie_watchlist_backend/MovieWatchlistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parsa.movie_watchlist_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieWatchlistBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
