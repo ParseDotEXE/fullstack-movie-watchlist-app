c5c6249187f8b07654fe6353dabc11f7ec33d35c
Parsa <parsarahimnia@gmail.com>
2025-06-28 12:49:23 -0400

    user constructor basics down

--- backend/src/main/java/com/parsa/movie_watchlist_backend/entity/User.java
+++ backend/src/main/java/com/parsa/movie_watchlist_backend/entity/User.java
@@ -1,5 +1,52 @@
 package com.parsa.movie_watchlist_backend.entity;
 
-public class User {
+import jakarta.persistence.*; //basic JPA annotations
+import org.hibernate.annotations.CreationTimestamp; //for timestamping
+import org.hibernate.annotations.UpdateTimestamp; //for timestamping
+import java.time.LocalDateTime; //for timestamping
+import java.util.Objects;  
 
+@Entity //indicates that this class is a JPA entity
+public class User {
+    @Id //indicates that this field is the primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto-generate the ID
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+
+    //TODO: constructors, getters, setters
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+    public User() {
+    }
+    //getters
+    public Long getId() {
+        return id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    //setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
