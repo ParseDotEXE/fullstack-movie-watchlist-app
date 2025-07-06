16852eeb6d438b47ea04562845f83de6d43d5c9b
Parsa <parsarahimnia@gmail.com>
2025-07-06 12:19:33 -0400

    UserRepo methods added

--- backend/src/main/java/com/parsa/movie_watchlist_backend/controller/UserController.java
+++ backend/src/main/java/com/parsa/movie_watchlist_backend/controller/UserController.java
@@ -12,7 +12,17 @@ public class UserController {
     // authenticate the user
    @PostMapping("/api/auth/login")
    public User signup(@RequestBody  signupData) {
-       //TODO: process POST request
+       //TODO:
+       //validate input data to see if email and password already exist
+       //create a new User object from SignupRequest data
+       //save user to database
+       //return the created user (without password)
+       //edge cases:
+         // - if email already exists, return an error
+         // - if password is too short and or does not meet criteria, return an error
+         //if username is already taken, return an error
+         //if there are empty fields, return an error
+
        
        return entity;
    }
@@ -20,7 +30,15 @@ public class UserController {
     // log in the user by email and password
    @PostMapping("/api/auth/login")
    public User login(@RequestBody loginData) {
-    
+         //TODO:
+         //find the user by email in database
+         //check if the user already exists
+         //verify password matches
+         //return user if valid (without password)
+         //return error if user does not exist or password is incorrect
+         //edge cases:
+         //user not found
+         //password does not match
    }
     
 }
--- backend/src/main/java/com/parsa/movie_watchlist_backend/repository/UserRepository.java
+++ backend/src/main/java/com/parsa/movie_watchlist_backend/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.parsa.movie_watchlist_backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.parsa.movie_watchlist_backend.entity.User;
+import java.util.*;
+
 public interface UserRepository extends JpaRepository<User, Long>{
-    //nned to find user in database -> findById(Long userId)
+    //nned to find user in database -> findByEmail
+    Optional<User> findByEmail(String email);
+    boolean existsByUsername(String username);
     //need to save user in database -> save(User user);
-    //need to save their email, username, and password
+    //use the built in User save(User user);
 }
